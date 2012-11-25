ec52cfea8a29f7621f8c1a38fa55a75bde563596
Hendra <hendra2392@gmail.com>
2012-11-26 01:19:14 +0800

    simultaneous input checking

--- game.js
+++ game.js
@@ -1,13 +1,21 @@
-var pingpong = {};
-pingpong.pressedKeys = [];
-var KEY = {
-    UP: 38,
-    DOWN: 40,
-    W: 87,
-    S: 83
-}
-
 $(document).ready(function(){
+    var KEY = {
+        UP: 38,
+        DOWN: 40,
+        W: 87,
+        S: 83
+    }
+
+    var pingpong = {};
+    pingpong.pressedKeys = [];
+    pingpong.ball = {
+        speed: 5,
+        x : 150,
+        y : 100,
+        directionX : 1,
+        directionY : 1
+    }
+
     pingpong.timer = setInterval(gameloop, 30);
 
     $(document).keydown(function(e){
@@ -19,7 +27,43 @@ $(document).ready(function(){
     });
 
     function gameloop(){
+        moveBall();
         movePaddles();
+    }
+
+    function moveBall(){
+        var playGroundHeight = parseInt($("#playground").height());
+        var playGroundWidth = parseInt($("#playground").width());
+        var ball = pingpong.ball;
+
+        // check bottom edge
+        if (ball.y + ball.speed * ball.directionY > playGroundHeight){
+            ball.directionY = -1;
+        }
+        // check top edge
+        if (ball.y + ball.speed * ball.directionY < 0){
+            ball.directionY = 1;
+        }
+        // check right edge
+        if( ball.x + ball.speed * ball.directionX > playGroundWidth){
+            ball.directionX = -1;
+        }
+        // check left edge
+        // check right edge
+        if( ball.x + ball.speed * ball.directionX < 0){
+            ball.directionX = 1;
+        }
+
+        ball.x += ball.speed * ball.directionX;
+        ball.y += ball.speed * ball.directionY;
+
+        //TODO: Check for paddle
+
+        // move the ball
+        $("#ball").css({
+            "left" : ball.x,
+            "top": ball.y
+        });
     }
 
     function movePaddles(){
