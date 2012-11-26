3583e24ec74364cd5aa4150032860793a3bd43c5
Hendra <hendra2392@gmail.com>
2012-11-26 11:18:07 +0800

    basic collision detection

--- game.js
+++ game.js
@@ -46,18 +46,56 @@ $(document).ready(function(){
         }
         // check right edge
         if( ball.x + ball.speed * ball.directionX > playGroundWidth){
+            // playerB lost
+            // reset the ball
+            ball.x = 250;
+            ball.y = 100;
+            $("#ball").css({
+                "top": ball.x,
+                "left": ball.y
+            });
             ball.directionX = -1;
         }
         // check left edge
         // check right edge
         if( ball.x + ball.speed * ball.directionX < 0){
+            // playerA lost
+            // reset the ball
+            ball.x = 250;
+            ball.y = 100;
+            $("#ball").css({
+                "top":ball.x,
+                "left":ball.y
+            });
             ball.directionX = 1;
         }
 
         ball.x += ball.speed * ball.directionX;
         ball.y += ball.speed * ball.directionY;
 
-        //TODO: Check for paddle
+        //check for left paddle
+        var paddleAX = parseInt($("#paddleA").css("left"))+parseInt($("#paddleA").css("width"));
+        var paddleAYBottom = parseInt($("#paddleA").css("top")) + parseInt($("#paddleA").css("height"));
+        var paddleAYTop = parseInt($("#paddleA").css("top"));
+
+        if(ball.x + ball.speed*ball.directionX < paddleAX){
+            if(ball.y+ball.speed*ball.directionY <= paddleAYBottom &&
+                ball.y + ball.speed*ball.directionY >= paddleAYTop){
+                ball.directionX = 1;
+            }
+        }
+
+        //check for right paddle
+        var paddleBX = parseInt($("#paddleB").css("left"))+parseInt($("#paddleB").css("width"));
+        var paddleBYBottom = parseInt($("#paddleB").css("top")) + parseInt($("#paddleB").css("height"));
+        var paddleBYTop = parseInt($("#paddleB").css("top"));
+
+        if(ball.x + ball.speed*ball.directionX >= paddleBX){
+            if(ball.y+ball.speed*ball.directionY <= paddleBYBottom &&
+                ball.y + ball.speed*ball.directionY >= paddleBYTop){
+                ball.directionX = -1;
+            }
+        }
 
         // move the ball
         $("#ball").css({
